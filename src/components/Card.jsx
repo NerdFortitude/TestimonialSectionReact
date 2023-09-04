d1a2aa9da12b260240bb7d621e2f473e2f19dd8a
Nishant Rai <rnishant9970@gmail.com>
2023-09-04 07:16:10 +0530

    Testimonail app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,16 +7,17 @@ const Card = (props)=>{
     return (
         <div className='flex flex-col relative '>
 
-            <div className='absolute top-[-7rem] z-[10] mx-auto'><img src={review.image} alt="" className='ascpect-square rounded-full w-[140px] h-[140px] z-[25]'/>
-             <div className='w-[140px] h-[140px] bg-violet-400 rounded-full absolute top-[-6px] z-[-10] left'></div>
+            <div className='absolute top-[-7rem] z-[20] mx-auto'><img src={review.image} alt="" className='ascpect-square rounded-full w-[140px] h-[140px] z-[25]'/>
+             <div className='w-[140px] h-[140px] bg-violet-400 rounded-full absolute top-[-8px] z-[-10] left-[8px]'></div>
             </div>
 
             <div className='text-center mt-7'>
                 <p className='font-bold text-2xl capitalize'>{review.name}</p>
+                <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
             </div>
 
             <div className='text-center mt-2'>
-                <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
+                
             </div>
 
             <div className='text-violet-400 mx-auto mt-5'><FaQuoteLeft></FaQuoteLeft></div>
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,20 +1,49 @@
 import Card from "./Card";
 import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
-
+import { useState } from "react";
 
 const Testimonial = (props)=>{
 
     let reviews = props.reviews;
+    const [index,setIndex] = useState(0);
+
+    function leftShiftHandler(){
+
+        if(index ===0){
+            setIndex(reviews.length-1);
+
+        }
+        else{
+            setIndex(index-1);
+        }
+
+    }
+
+    function rightShiftHandler(){
+        if(index ===reviews.length-1){
+            setIndex(0);
+
+        }
+        else{
+            setIndex(index+1);
+        }
+    }
+
+    function supriseHandler(){
+
+        setIndex((Math.floor(Math.random()*reviews.length)));
+
+    }
 
     return (
-        <div>
-            <Card review = {reviews[0]}></Card>
+        <div className="w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-lg">
+            <Card review = {reviews[index]}></Card>
             <div className='flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto'>
                 <button className='cursor-pointer hover:text-violet-500' onClick={leftShiftHandler}> <FiChevronLeft></FiChevronLeft></button>
                 <button className='cursor-pointer hover:text-violet-500' onClick={rightShiftHandler}> <FiChevronRight></FiChevronRight></button>
             </div>
 
-            <div>
+            <div className="mt-5">
                 <button className='bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg' onClick={supriseHandler}>Surprise Me</button>
             </div>
         </div>
